c5eac73b287c9739f6cecb10f6569bda4e9e12d2
auto-nic <anton.lundberg@gmail.com>
2025-05-17 14:16:31 +0200

    fix

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -38,10 +38,7 @@ use Illuminate\Support\Facades\Route;
 
         <!-- Remember Me -->
         <div>
-            <label for="remember" class="inline-flex items-center">
-                <input wire:model="remember" id="remember" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Kom ihåg mig</span>
-            </label>
+            
         </div>
 
         <div class="flex items-center justify-end">
