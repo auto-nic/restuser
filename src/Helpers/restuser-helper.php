8406ae05aec5fbd21efeb7ac3e5c307dcbbe63f8
auto-nic <anton.lundberg@gmail.com>
2025-05-22 21:15:49 +0200

    fix

--- src/Helpers/restuser-helper.php
+++ src/Helpers/restuser-helper.php
@@ -24,3 +24,21 @@ if (!function_exists('isMobile')) {
         return false;
     }
 }
+
+if (!function_exists('get_customer_setting')) {
+    function get_customer_setting(string $name) {
+
+        if (!auth()->customerId()) throw new \Exception('Customer ID is not set for the authenticated user.');
+
+        if (empty($name)) throw new \Exception('get_customer_setting() requires a name parameter.');
+
+        $setting = \App\Models\CustomerSetting::where('customer_id', auth()->customerId())->first();
+
+        if (!$setting) throw new \Exception('Customer settings not found for the authenticated user.');
+
+        if (!property_exists($setting, $name)) throw new \Exception('There is no customer setting with the name: ' . $name);
+
+        return $setting->$name;
+
+    }
+}
